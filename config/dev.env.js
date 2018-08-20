1dd698824a067245ef9bf9eb59788e0042c0a7e6
inin88_rongtianyang <rty753951>
2018-08-20 14:27:21 +0800

    配置pro/dev环境

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,9 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
+"use strict";
+const merge = require("webpack-merge");
+const prodEnv = require("./prod.env");
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
-})
+  NODE_ENV: '"development"',
+  BASE_URL:
+    '"https://www.easy-mock.com/mock/5b61451dcbc4e209c427f9fc/dev_api_v1"'
+});
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,5 @@
-'use strict'
+"use strict";
 module.exports = {
-  NODE_ENV: '"production"'
-}
+  NODE_ENV: '"production"',
+  BASE_URL: '"https://www.easy-mock.com/mock/5b6142e7cbc4e209c427f9f4/api_v1"'
+};
